40898cbd02fedfa1e4909c146ea766a091d2774d
tbreffni <tbreffni@gmail.com>
2015-09-24 15:11:40 -0400

    Update forwarder to reflect new constructor pattern

--- Inspectlet.js
+++ Inspectlet.js
@@ -8,125 +8,134 @@
         OptOut: 6,
         Commerce: 16
     },
-    isInitialized = false,
-    forwarderSettings,
-    name = 'Inspectlet',
-    reportingService,
-    id = null,
-    isTesting = false;
+    name = 'Inspectlet';
 
-    function getIdentityTypeName(identityType) {
-        return mParticle.IdentityType.getName(identityType);
-    }
+    var constructor = function () {
+        var self = this,
+            isInitialized = false,
+            forwarderSettings,
+            reportingService,
+            id = null,
+            isTesting = false;
 
-    function reportEvent(event) {
-        if(reportingService) {
-            reportingService(id, event);
+        function getIdentityTypeName(identityType) {
+            return mParticle.IdentityType.getName(identityType);
         }
-    }
 
-    function processEvent(event) {
-        if (isInitialized) {
-            try {
-                if (event.EventDataType == MessageType.PageEvent) {
-                    if (event.EventCategory == window.mParticle.EventType.Transaction) {
+        function reportEvent(event) {
+            if(reportingService) {
+                reportingService(id, event);
+            }
+        }
+
+        function processEvent(event) {
+            if (isInitialized) {
+                try {
+                    if (event.EventDataType == MessageType.PageEvent) {
+                        if (event.EventCategory == window.mParticle.EventType.Transaction) {
+                            logTransaction(event);
+                            reportEvent(event);
+                            return 'Successfully sent to ' + name;
+                        }
+                        else if (event.EventCategory == window.mParticle.EventType.Navigation) {
+                            __insp.push(["virtualPage"]);
+                            reportEvent(event);
+
+                            return 'Successfully sent virtual page view to ' + name;
+                        }
+                    }
+                    else if(event.EventDataType == MessageType.Commerce) {
                         logTransaction(event);
-                        reportEvent(event);
-                        return 'Successfully sent to ' + name;
                     }
-                    else if (event.EventCategory == window.mParticle.EventType.Navigation) {
-                        __insp.push(["virtualPage"]);
-                        reportEvent(event);
 
-                        return 'Successfully sent virtual page view to ' + name;
-                    }
+                    return 'Ignoring non-transaction event for ' + name;
                 }
-                else if(event.EventDataType == MessageType.Commerce) {
-                    logTransaction(event);
+                catch (e) {
+                    return 'Failed to send to: ' + name + ' ' + e;
                 }
-
-                return 'Ignoring non-transaction event for ' + name;
             }
-            catch (e) {
-                return 'Failed to send to: ' + name + ' ' + e;
-            }
+
+            return 'Can\'t send to forwarder ' + name + ', not initialized';
         }
 
-        return 'Can\'t send to forwarder ' + name + ', not initialized';
-    }
-
-    function logTransaction(data) {
-        __insp.push(['tagSession', "purchase"]);
-    }
-
-    function setUserAttribute(key, value) {
-        var attributeDict;
-
-        if (isInitialized) {
-            if (value) {
-                attributeDict = {};
-                attributeDict[key] = value;
-                __insp.push(['tagSession', attributeDict]);
-            }
-            else {
-                __insp.push(['tagSession', key]);
-            }
+        function logTransaction(data) {
+            __insp.push(['tagSession', "purchase"]);
         }
-        else {
-            return 'Can\'t call setUserAttribute on forwarder ' + name + ', not initialized';
-        }
-    }
 
-    function setUserIdentity(id, type) {
-        if (isInitialized) {
-            setUserAttribute(getIdentityTypeName(type), id);
-        }
-        else {
-            return 'Can\'t call setUserIdentity on forwarder ' + name + ', not initialized';
-        }
-    }
+        function setUserAttribute(key, value) {
+            var attributeDict;
 
-    function initForwarder(settings, service, moduleId, testMode) {
-        forwarderSettings = settings;
-        id = moduleId;
-        reportingService = service;
-        isTesting = testMode;
-
-        try {
-            function addInspectlet() {
-                function __ldinsp() {
-                    var insp = document.createElement('script');
-                    insp.type = 'text/javascript';
-                    insp.async = true;
-                    insp.id = "inspsync";
-                    insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js';
-                    var head = document.getElementsByTagName('head')[0];
-                    head.appendChild(insp);
-                }
-
-                if (window.attachEvent) {
-                    window.attachEvent('onload', __ldinsp);
+            if (isInitialized) {
+                if (value) {
+                    attributeDict = {};
+                    attributeDict[key] = value;
+                    __insp.push(['tagSession', attributeDict]);
                 }
                 else {
-                    window.addEventListener('load', __ldinsp, false);
+                    __insp.push(['tagSession', key]);
                 }
             }
-
-            window.__insp = window.__insp || [];
-            __insp.push(['wid', forwarderSettings.WId]);
-
-            if(isTesting === false) {
-                addInspectlet();
+            else {
+                return 'Can\'t call setUserAttribute on forwarder ' + name + ', not initialized';
             }
-
-            isInitialized = true;
-
-            return 'Successfully initialized: ' + name;
         }
-        catch (e) {
-            return 'Failed to initialize: ' + name;
+
+        function setUserIdentity(id, type) {
+            if (isInitialized) {
+                setUserAttribute(getIdentityTypeName(type), id);
+            }
+            else {
+                return 'Can\'t call setUserIdentity on forwarder ' + name + ', not initialized';
+            }
         }
-    }
+
+        function initForwarder(settings, service, moduleId, testMode) {
+            forwarderSettings = settings;
+            id = moduleId;
+            reportingService = service;
+            isTesting = testMode;
+
+            try {
+                function addInspectlet() {
+                    function __ldinsp() {
+                        var insp = document.createElement('script');
+                        insp.type = 'text/javascript';
+                        insp.async = true;
+                        insp.id = "inspsync";
+                        insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js';
+                        var head = document.getElementsByTagName('head')[0];
+                        head.appendChild(insp);
+                    }
+
+                    if (window.attachEvent) {
+                        window.attachEvent('onload', __ldinsp);
+                    }
+                    else {
+                        window.addEventListener('load', __ldinsp, false);
+                    }
+                }
+
+                window.__insp = window.__insp || [];
+                __insp.push(['wid', forwarderSettings.WId]);
+
+                if(isTesting === false) {
+                    addInspectlet();
+                }
+
+                isInitialized = true;
+
+                return 'Successfully initialized: ' + name;
+            }
+            catch (e) {
+                return 'Failed to initialize: ' + name;
+            }
+        }
+
+        this.init = initForwarder;
+        this.process = processEvent;
+        this.setUserAttribute = setUserAttribute;
+        this.setUserIdentity = setUserIdentity;
+    };
 
     if (!window || !window.mParticle || !window.mParticle.addForwarder) {
         return;
@@ -134,10 +143,7 @@
 
     window.mParticle.addForwarder({
         name: name,
-        init: initForwarder,
-        process: processEvent,
-        setUserAttribute: setUserAttribute,
-        setUserIdentity: setUserIdentity
+        constructor: constructor
     });
 
 })(window);
